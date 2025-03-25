38b04305274b48cab42894e0e7f91ff1ce4b1960
eugebajo <bae23001@byui.edu>
2025-03-25 12:23:18 -0400

    javaScript added

---
+++ scripts/temples.js
@@ -0,0 +1,11 @@
+// Get current year and last modified date
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = document.lastModified;
+
+// Hamburger Menu Toggle
+const hamburgerButton = document.getElementById('hamburger-btn');
+const navMenu = document.getElementById('nav-menu');
+
+hamburgerButton.addEventListener('click', function() {
+    navMenu.classList.toggle('show');
+});
