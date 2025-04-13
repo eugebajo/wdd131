255f1e4eefca7f762c7f53b72938420c93f846c0
eugebajo <bae23001@byui.edu>
2025-04-13 16:51:06 -0400

    place added

--- scripts/place.js
+++ scripts/place.js
@@ -1,21 +1,25 @@
+// Variables estáticas
+const tempC = 8; // temperatura en °C
+const windKmh = 10; // velocidad del viento en km/h
+
 function calculateWindChill(temp, windSpeed) {
-    return (
-      13.12 +
-      0.6215 * temp -
-      11.37 * Math.pow(windSpeed, 0.16) +
-      0.3965 * temp * Math.pow(windSpeed, 0.16)
-    ).toFixed(1);
-  }
-  
-  // Mostrar sensación térmica si aplica
-  const windChillSpan = document.getElementById("windchill");
-  
-  if (tempC <= 10 && windKmh > 4.8) {
-    windChillSpan.textContent = calculateWindChill(tempC, windKmh) + " °C";
-  } else {
-    windChillSpan.textContent = "N/A";
-  }
-  
-  // Año actual y fecha de modificación
-  document.getElementById("year").textContent = new Date().getFullYear();
-  document.getElementById("modDate").textContent = document.lastModified;
+  return (
+    13.12 +
+    0.6215 * temp -
+    11.37 * Math.pow(windSpeed, 0.16) +
+    0.3965 * temp * Math.pow(windSpeed, 0.16)
+  ).toFixed(1);
+}
+
+// Mostrar sensación térmica si aplica
+const windChillSpan = document.getElementById("windchill");
+
+if (tempC <= 10 && windKmh > 4.8) {
+  windChillSpan.textContent = calculateWindChill(tempC, windKmh) + " °C";
+} else {
+  windChillSpan.textContent = "N/A";
+}
+
+// Año actual y fecha de última modificación
+document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("modDate").textContent = document.lastModified;
